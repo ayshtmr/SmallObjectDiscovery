6a0e52d7fbaac0084fb6052623a1176e11a933f8
Ayush <ayushtomar@gmail.com>
2015-01-11 04:52:01 +0530

    PCL added

--- src/test1.cpp
+++ src/test1.cpp
@@ -25,6 +25,33 @@
 #include <actionlib_msgs/GoalStatusArray.h>
 #include <actionlib/client/simple_action_client.h>
 #include <move_base_msgs/MoveBaseAction.h>
+#include <pcl/point_types.h>
+#include <pcl_ros/point_cloud.h>
+#include <ros/ros.h>
+#include <boost/foreach.hpp>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/common/common.h>
+#include <pcl/common/transforms.h>
+#include <pcl/console/parse.h>
+#include <pcl/console/print.h>
+#include <flann/flann.h>
+#include <flann/io/hdf5.h>
+#include <boost/filesystem.hpp>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/vfh.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/project_inliers.h>
+#include <pcl/surface/convex_hull.h>
+#include <pcl/segmentation/extract_polygonal_prism_data.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/filters/statistical_outlier_removal.h>
 
 #define NO_CLUSTERS 8
 #define ATTEMPTS 10
@@ -41,9 +68,19 @@ typedef union U_FloatParse {
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+typedef pcl::PointXYZRGB Point1;
+typedef pcl::PointCloud<pcl::PointXYZRGB> cloudxyz;
+typedef cloudxyz::Ptr cloudptr;
+typedef pcl::search::KdTree<Point1>::Ptr KdTreePtr;
+
+cloudxyz PC;
+string object_ID;
+int kn=0;
+
+
 sensor_msgs::ImageConstPtr msga,msgc;
 nav_msgs::OccupancyGrid msgb;
-actionlib_msgs::GoalStatusArrayConstPtr msgd;
+sensor_msgs::PointCloud2::ConstPtr msgd;
 //ros::Publisher nextgoal_pub;
 
 
@@ -154,12 +191,132 @@ void depthcallback( const sensor_msgs::ImageConstPtr &msg1)
     std::cout<<endl<<"Depth <-";
 
 }
-void actioncallback(const actionlib_msgs::GoalStatusArrayConstPtr &msg3)
+
+void pclcallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 {
-    msgd=msg3;
+    msgd=msg;
 
     //std::cout<<endl<<"Status <-";
     flag4=1;  
+}
+
+cv::Mat image(640,480,CV_8UC3,cv::Scalar(0));
+
+
+void callback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+    cloudptr cloud_in (new cloudxyz());
+    pcl::fromROSMsg(*msg, *cloud_in);
+    
+    cloudptr cloud (new cloudxyz());
+
+    pcl::PassThrough<Point1> pass_;
+    pass_.setFilterFieldName("z");
+    pass_.setFilterLimits(0.0, 3.0);
+    pass_.setInputCloud(cloud_in);
+    pass_.filter(*cloud);
+
+    
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
+    cloud_filtered=cloud;   
+    
+    // Create the segmentation object for the planar model and set all the parameters
+    pcl::SACSegmentation<pcl::PointXYZRGB> seg;
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZRGB> ());
+    pcl::PCDWriter writer;
+    seg.setOptimizeCoefficients (true);
+    seg.setModelType (pcl::SACMODEL_PERPENDICULAR_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setAxis(Eigen::Vector3f(0,-1,0));
+        seg.setEpsAngle(  40.0f * (M_PI/180.0f) );
+    seg.setMaxIterations (100);
+    seg.setDistanceThreshold (0.02);
+
+    
+        // Segment the largest planar component from the remaining cloud
+        seg.setInputCloud (cloud_filtered);
+        seg.segment (*inliers, *coefficients);
+        
+        // Extract the planar inliers from the input cloud
+        pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+        extract.setInputCloud (cloud_filtered);
+        extract.setIndices (inliers);
+        extract.setNegative (false);
+
+        // Write the planar inliers to disk
+        extract.filter (*cloud_plane);
+        //std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size () << " data points." << std::endl;
+
+        // Remove the planar inliers, extract the rest
+        extract.setNegative (true);
+        extract.filter (*cloud_f);
+        cloud_filtered = cloud_f;
+    
+
+    //Remove the outliers
+    cloudptr object (new cloudxyz());
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor;
+    sor.setInputCloud (cloud_filtered);
+    sor.setMeanK(50);
+    sor.setStddevMulThresh (1.0);
+    sor.filter(*object);
+
+    //vfh estimation of the each object cluster
+
+    pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal> ());
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr normals_tree_ (new pcl::search::KdTree<pcl::PointXYZRGB> ());
+    pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normal_;
+    normal_.setKSearch(10);
+    normal_.setSearchMethod(normals_tree_);
+    normal_.setInputCloud(object);
+    normal_.compute(*normals);
+    //      cout<<"Normals for vfh is computed"<<endl;
+        cout<<"The vfh signature are computed"<<endl;
+
+    cv::Mat image_1(640,480,CV_8UC3,cv::Scalar(0));
+    pcl::PointCloud<pcl::PointXYZRGB> cloud_1;
+    
+    cout<<cloud_plane->points.size()<<endl; 
+    cv::Mat occ(480,640,CV_8UC1,cv::Scalar(0));
+
+    float x=0,y=0,z=0;
+    float image_x=0; 
+    float image_y=0;
+    float focus=525;
+    float cx=319.5;
+    float cy=239.5;
+    float f=5000;
+    cout<<"hi "<<cloud_plane->points.size();
+    for(int i=1; i<cloud_plane->points.size(); i++)
+    {
+        x=cloud_plane->points[i].x;
+        y=cloud_plane->points[i].y;
+        z=cloud_plane->points[i].z;
+        //cout << "     "<< x<<"    "<<y<<"     "<<z<<endl;
+        image_x=x/z*focus;
+        image_y=y/z*focus;
+        //cout <<"image_x:  "<<image_x<<"   image_y:    "<<image_y<<endl;   
+        image_x=cx+image_x; 
+        image_y=cy+image_y; 
+
+        occ.at<uchar>(image_y,image_x)=255;
+        
+
+    }
+
+    if(cloud_plane->points.size () > 40000)
+    {
+    cv::imwrite("pc.jpg",occ);
+    }
+    
+    //k++;
+    
+    sleep(5);
+    cout<<"sleeping";
 }
 
 
@@ -664,11 +821,12 @@ int loop1=0,loop2=0;
 int depth=9999;
 actionlib_msgs::GoalStatus goalStatus;
 
-void callit(const sensor_msgs::ImageConstPtr msg3, const nav_msgs::OccupancyGrid msg4, const sensor_msgs::ImageConstPtr& msg5)//, const actionlib_msgs::GoalStatusArrayConstPtr& msg6)
+void callit(const sensor_msgs::ImageConstPtr msg3, const nav_msgs::OccupancyGrid msg4, const sensor_msgs::ImageConstPtr& msg5, const sensor_msgs::PointCloud2::ConstPtr& msg6)
 {  
-    // flag4=0;
+    flag4=0;
     flag3=0;
     flag2=0;
+    callback(msg6);
     //flag1=0;
     loop1++;
     if(explore_var){
@@ -707,14 +865,30 @@ void callit(const sensor_msgs::ImageConstPtr msg3, const nav_msgs::OccupancyGrid
     int max_thresh = 255;
     RNG rng(12345);
 
-    src=imread("blob.jpg");
-    cout<<endl<<"blob image generated"<<endl;
-    
-    cvtColor( src, src_gray, CV_BGR2GRAY );
+
+    src_gray=imread("blob.jpg", 0);
+    //cvtColor( src, src, CV_BGR2GRAY );
     blur( src_gray, src_gray, Size(3,3) );
+    cout<<endl<<"blob image generated"<<endl;
+
+    Mat src1;
+    src1=imread("pc.jpg",0);
+     Mat elem=getStructuringElement(MORPH_RECT,Size(15,15),Point(7,7));
+     dilate( src1, src1, elem );
+   // cout<<"blob channels "<<src_gray.rows<<" "<<src_gray.cols<<" pc channels "<<src1.rows<<" "<<src1.cols;
+
+     bitwise_and(src1,src_gray,src_gray);
+     imwrite("rr.jpg",src_gray);
+     waitKey(100);
+
+    //src=imread("blob.jpg");
+    //cout<<endl<<"blob image generated"<<endl;
+    
+
     Mat canny_output;
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
+
 
 
     Canny( src_gray, canny_output, thresh, thresh*2, 3 );
@@ -895,7 +1069,8 @@ int main(int argc, char** argv)
     image_transport::ImageTransport it1(n1);
     image_transport::Subscriber sub2=it1.subscribe("/camera/rgb/image_color",1,callback);
     image_transport::Subscriber sub = it.subscribe("camera/depth/image", 1, depthcallback);
-    ros::Subscriber sub3=n1.subscribe("map",1,goalCallback);   
+    ros::Subscriber sub3=n1.subscribe("map",1,goalCallback); 
+    ros::Subscriber sub4 = n1.subscribe("camera/depth_registered/points", 1, pclcallback);  
     //ros::Subscriber sub4=n1.subscribe<actionlib_msgs::GoalStatusArray>("/move_base/status", 1, actioncallback);
     //pub2 = n1.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal",1);
 
@@ -903,8 +1078,8 @@ int main(int argc, char** argv)
     {
         ros::spinOnce();
 
-        if(flag1&&flag2&&flag3)//&&flag4)
-            callit(msga,msgb,msgc);//,msgd);
+        if(flag1&&flag2&&flag3&&flag4)
+            callit(msga,msgb,msgc,msgd);
 
         ros::Rate r(30);
         r.sleep();
